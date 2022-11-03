c1cb2164126838e75ac530f91e87b1599974400c
puterphile <puterphile@gmail.com>
2022-11-03 06:57:39 +0300

    chore(benchmark): benchmark latest version of classis in npm registery

--- benchmark/index.ts
+++ benchmark/index.ts
@@ -1,12 +1,22 @@
 import { Suite } from "benchmark";
 import classis from "..";
+import _classis from "@scriptum/classis";
 
 new Suite()
+  .add("_classis conditaion", () => {
+    _classis(true && "blue", false && "yellow");
+  })
   .add("classis conditaion", () => {
     classis(true && "blue", false && "yellow");
   })
+  .add("_classis undefined", () => {
+    _classis(undefined, ["blue", [undefined]], {});
+  })
   .add("classis undefined", () => {
     classis(undefined, ["blue", [undefined]], {});
+  })
+  .add("_classis override", () => {
+    _classis("blue", "white", { white: "yellow" });
   })
   .add("classis override", () => {
     classis("blue", "white", { white: "yellow" });
